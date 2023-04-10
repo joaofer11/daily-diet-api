ba5da5ace2363840a7469f980bc7874a69a65478
João Fernandes <joaofergear003@gmail.com>
2023-04-10 19:53:58 -0300

    test(meals-routes): add test for meals getting

--- test/meals.test.ts
+++ test/meals.test.ts
@@ -7,6 +7,31 @@ describe('Meals Routes', () => {
 
   afterAll(async () => app.close());
 
+  it('should be able to list all meals', async () => {
+    const createMealRes = await request(app.server)
+      .post('/meals')
+      .send({
+        name: 'Refeição test',
+        description: 'Refeição N/A de test',
+        isUnderDiet: false
+      });
+
+    const cookies = createMealRes.get('Set-Cookie');
+
+    const listMealsRes = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200);
+
+    expect(listMealsRes.body.meals).toEqual([
+      expect.objectContaining({
+        name: 'Refeição test',
+        description: 'Refeição N/A de test',
+        is_under_diet: 0
+      })
+    ]);
+  });
+
   it('should be able to create a meal', async () => {
     const createMealRes = await request(app.server)
       .post('/meals')
